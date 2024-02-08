670496a509add230ec329c8717168aa85bb95047
Daniil Khokhlov <danhoh228@mail.ru>
2024-02-08 19:18:34 +0300

    Managed to connect to database

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    final static private String url = "jdbc:mysql://localhost:3306/sakila";
+    final static private String user = "root";
+    final static private String password = "1234567890";
+    public static void connect() {
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Успешное подключение к базе данных MySQL!");
+            // Здесь можно выполнять SQL-запросы и работать с результатами
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения к базе данных MySQL: " + e.getMessage());
+        }
+    }
 }
